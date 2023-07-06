ff136f69fa8c86a7cde0808b9d33e337720c9c15
Mujahid <mujahidghazalgoo@gmail.com>
2023-07-06 16:20:11 -0400

    phase-3.2

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,9 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
 fetch('/api/session', {
@@ -27,5 +32,39 @@ fetch('/api/session', {
     },
     body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
   }).then(res => res.json()).then(data => console.log(data));
+
+  fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+    },
+    body: JSON.stringify({
+      email: 'spidey@spider.man',
+      username: 'Spidey',
+      password: 'password'
+    })
+  }).then(res => res.json()).then(data => console.log(data));
+
+  router.post(
+    '/',
+    async (req, res) => {
+      const { email, password, username } = req.body;
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({ email, username, hashedPassword });
+
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+
+      await setTokenCookie(res, safeUser);
+
+      return res.json({
+        user: safeUser
+      });
+    }
+  );
 
 module.exports = router;
