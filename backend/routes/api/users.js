81710027c2403a0cbb6a6031db176d4ac920b691
Mujahid <mujahidghazalgoo@gmail.com>
2023-07-06 16:41:54 -0400

    phase-4

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,47 +5,6 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
-
-fetch('/api/session', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-    },
-    body: JSON.stringify({ credential: 'Demo-lition', password: 'password' })
-  }).then(res => res.json()).then(data => console.log(data));
-
-  fetch('/api/session', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-    },
-    body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
-  }).then(res => res.json()).then(data => console.log(data));
-
-  fetch('/api/session', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-    },
-    body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
-  }).then(res => res.json()).then(data => console.log(data));
-
-  fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-    },
-    body: JSON.stringify({
-      email: 'spidey@spider.man',
-      username: 'Spidey',
-      password: 'password'
-    })
-  }).then(res => res.json()).then(data => console.log(data));
-
   router.post(
     '/',
     async (req, res) => {
