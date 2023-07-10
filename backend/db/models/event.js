39e45a73fe549a73ba04576b4fee1cd9a0830f1b
Mujahid <mujahidghazalgoo@gmail.com>
2023-07-10 18:03:57 -0400

    made tables

---
+++ backend/db/models/event.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Event.init({
+    venueId: DataTypes.INTEGER,
+    groupId: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    type: DataTypes.ENUM,
+    capacity: DataTypes.INTEGER,
+    price: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Event',
+  });
+  return Event;
+};
