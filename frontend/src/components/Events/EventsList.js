1f3753b51e2267c38d7e625af974419ff88f295f
Mujahid <mujahidghazalgoo@gmail.com>
2023-08-31 00:02:43 -0400

    css

--- frontend/src/components/Events/EventsList.js
+++ frontend/src/components/Events/EventsList.js
@@ -1,27 +1,28 @@
-import {Link} from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { retrieveAllEvents } from "../../store/events";
-import './EventsList.css';
+import "./EventsList.css";
 
 const EventsList = () => {
-    const events = Object.values(
-        useSelector((state) => (state.events.allEvents))
-    );
-    const dispatch = useDispatch();
+  const events = Object.values(useSelector((state) => state.events.allEvents));
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(retrieveAllEvents());
-    }, [dispatch])
-    return (
-        <div className="container">
-      <ul>
-        <h1 className="title">
-          <Link className="link" to="/groups">Groups</Link>
-          <p>Events</p>
-        </h1>
-      </ul>
-
+  useEffect(() => {
+    dispatch(retrieveAllEvents());
+  }, [dispatch]);
+  return (
+    <div className="container">
+      <div className="titleGroupsList">
+        <div className="titleLink">
+          <Link className="link" to="/groups">
+            Groups
+          </Link>
+        </div>
+        <div className="titleLink">
+          <Link>Events</Link>
+        </div>
+      </div>
       <div>
         {events.map((event) => (
           <div key={event.id} className="event-item">
@@ -31,11 +32,12 @@ const EventsList = () => {
             <p>{event.Group.city}</p>
             <p>{event.Group.state}</p>
             <p>{event.description}</p>
+            <hr></hr>
           </div>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default EventsList;
