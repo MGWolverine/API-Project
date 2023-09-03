71befaab8a3f52ffcdc284a89012fada13049739
Mujahid <mujahidghazalgoo@gmail.com>
2023-09-03 18:51:02 -0400

    css

--- frontend/src/components/Events/EventDetails.js
+++ frontend/src/components/Events/EventDetails.js
@@ -5,6 +5,8 @@ import { retrieveSingleEvent } from "../../store/events";
 import { Link } from "react-router-dom";
 import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
 import DeleteEvent from "./DeleteEvent";
+import "./EventsList.css";
+import "../Groups/GroupsList.css";
 
 const EventDetails = () => {
   const { eventId } = useParams();
@@ -38,47 +40,53 @@ const EventDetails = () => {
         &lt; Events
       </Link>
       <h2>{singleEvent.name}</h2>
-      <p>Hosted by First Name Last Name</p>
-      <div className="group-details">
-        <div>
-          <img
-            className="groupDetailsImage"
-            src={singleEvent.EventImages[0].url}
-          />
+      <p>Hosted by Fulan Ibn Fulan</p>
+      <div className="mainbody-div">
+        <div className="group-details2">
+          <div className="group-details-image-div">
+            <img
+              className="groupDetailsImage"
+              src={singleEvent.EventImages[0].url}
+            />
+          </div>
+          <div>
+            <div>
+              <img></img>
+              <p>{singleEvent.Group.name}</p>
+              <p>
+                {singleEvent.Group.private === false ? "Public" : "Private"}
+              </p>
+            </div>
+            <div className="group-details-information">
+              <p>Start: {formatDate(singleEvent.startDate)} · 9AM</p>
+              <p>End: {formatDate(singleEvent.endDate)} · 10AM</p>
+              <p>Price: ${singleEvent.price}</p>
+              <p>Type: {singleEvent.type}</p>
+              {singleEvent.Group.organizerId != sessionUser.id && (
+                <button onClick={() => alert("Feature coming soon...")}>
+                  Join this Event
+                </button>
+              )}
+              {singleEvent.Group.organizerId === sessionUser.id && (
+                <Link to={`/${singleEvent.id}/edit`}>Manage Event</Link>
+              )}
+              {singleEvent.Group.organizerId === sessionUser.id && (
+                <button>
+                  <OpenModalMenuItem
+                    buttonText="Delete"
+                    modalComponent={<DeleteEvent eventId={singleEvent.id} />}
+                  />
+                  Delete
+                </button>
+              )}
+            </div>
+          </div>
         </div>
         <div>
-          <img></img>
-          <p>{singleEvent.Group.name}</p>
-          <p>{singleEvent.Group.private === false ? "Public" : "Private"}</p>
-        </div>
-        <div>
-          <p>Start: {formatDate(singleEvent.startDate)} · 9AM</p>
-          <p>End: {formatDate(singleEvent.endDate)} · 10AM</p>
-          <p>Price: ${singleEvent.price}</p>
-          <p>Type: {singleEvent.type}</p>
-          {singleEvent.Group.organizerId != sessionUser.id && (
-            <button onClick={() => alert("Feature coming soon...")}>
-              Join this Event
-            </button>
-          )}
-          {singleEvent.Group.organizerId === sessionUser.id && (
-            <Link to={`/${singleEvent.id}/edit`}>Manage Event</Link>
-          )}
-          {singleEvent.Group.organizerId === sessionUser.id && (
-            <button>
-              <OpenModalMenuItem
-                buttonText="Delete"
-                modalComponent={<DeleteEvent eventId={singleEvent.id} />}
-              />
-              Delete
-            </button>
-          )}
-        </div>
-      </div>
-      <div>
-        <div>
-          <h2>Details</h2>
-          <p>{singleEvent.description}</p>
+          <div>
+            <h2>Details</h2>
+            <p>{singleEvent.description}</p>
+          </div>
         </div>
       </div>
     </>
--- frontend/src/components/Events/EventsList.js
+++ frontend/src/components/Events/EventsList.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { retrieveAllEvents } from "../../store/events";
 import "./EventsList.css";
+import "../Groups/GroupsList.css";
 import { useHistory } from "react-router-dom";
 
 const EventsList = () => {
@@ -13,6 +14,15 @@ const EventsList = () => {
     )
   );
   const dispatch = useDispatch();
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  }
 
   useEffect(() => {
     dispatch(retrieveAllEvents());
@@ -39,12 +49,23 @@ const EventsList = () => {
               history.push(`/events/${event.id}`);
             }}
           >
-            <img src={event.previewImage} alt={event.name} />
-            <h2>{event.name}</h2>
-            <p>{event.startDate}</p>
-            <p>{event.Group.city}</p>
-            <p>{event.Group.state}</p>
-            <p>{event.description}</p>
+            <div className="groupContainer">
+              <div className="groupImageDiv">
+                <img
+                  className="groupImage"
+                  src={event.previewImage}
+                  alt={event.name}
+                />
+              </div>
+              <div className="groupInfo">
+                <p>{formatDate(event.startDate)} · 9:30 AM</p>
+                <h2>{event.name}</h2>
+                <p className="groupsListp">
+                  {event.Group.city}, {event.Group.state}
+                </p>
+              </div>
+            </div>
+            <p className="groupsListp">{event.description}</p>
             <hr></hr>
           </div>
         ))}
