3d0c86ea7a8a4a107340ea1b2f4148a0af2d21a1
Mujahid <mujahidghazalgoo@gmail.com>
2023-10-01 19:35:24 -0400

    fixed groups list private field

--- frontend/src/components/Groups/GroupsList.js
+++ frontend/src/components/Groups/GroupsList.js
@@ -55,7 +55,7 @@ const GroupsList = () => {
                 <p className="groupsListp">{group.about}</p>
               </div>
             </div>
-            <p>{group?.Events?.length} event · Public/Private </p>
+            <p>{group?.Events?.length} event · {group?.private === false ? "Public" : "Private"} </p>
             <hr></hr>
           </div>
         ))}
